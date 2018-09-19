d8f9c0bf9cac53fa3ead291345e3308c19f6e3aa
David Mueller <dave.mueller@gmx.net>
2018-09-19 13:05:52 +0200

    Created close button, added close confirmation

--- GUI_PyQt5.py
+++ GUI_PyQt5.py
@@ -5,7 +5,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtGui import QIcon
+from PyQt5.Qt import center
 
 
 class Example(QWidget):
@@ -26,10 +29,28 @@ class Example(QWidget):
         self.move(300, 300)
         self.setWindowTitle('My PyQt5 GUI')
         self.setWindowIcon(QIcon('web.png'))
+        
+        # Create pushbutton that closes the window
+        btn = QPushButton('Exit', self)
+        btn.clicked.connect(QApplication.instance().quit)
+        btn.resize(btn.sizeHint())
+        btn.move(0, 0)  
+        
         # Show the already created window now on screen
         self.show()
-        
-        
+    
+    # Ask for confirmation on closeEvent (press of X in window title)
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Confirm closeEvent',
+            "Are you sure to quit?", QMessageBox.Yes | 
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+    
+
 if __name__ == '__main__':
     
     # Create application object
