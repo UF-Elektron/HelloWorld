be1fe708d7844d6233ceccc252dfa3c194d31cc1
David Mueller <dave.mueller@gmx.net>
2018-09-20 11:04:22 +0200

    Added new class for slider window, added comments

--- GUI_PyQt5.py
+++ GUI_PyQt5.py
@@ -10,9 +10,37 @@ from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QLCDNumber
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QSlider
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 
-
+class SliderWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        # Setup of slider
+        slider = QSlider(Qt.Horizontal, self)
+        slider.setMinimum(20)
+        slider.setMaximum(140)
+        
+        # Setup of number display
+        lcd = QLCDNumber()
+        slider.valueChanged.connect(lcd.display)
+        
+        slider.setValue(80)
+                
+        layout = QVBoxLayout()
+        layout.addWidget(lcd)
+        layout.addWidget(slider)
+        
+        # Set size of the window
+        self.resize(300, 350)
+        # Set position on the screen
+        self.move(50, 50)
+        self.setWindowTitle('Slider Window')
+        
+        self.setLayout(layout)
+        
 class NewWindow(QWidget):
     myCounter = 0
     lcd = 0
@@ -43,17 +71,21 @@ class NewWindow(QWidget):
         print('Visibility = ', self.isVisible())
         return self.isVisible()
         
+    # Increment counter and update LCD in window
     def add(self):
         self.myCounter = self.myCounter + 1
         self.lcd.display(self.myCounter)
         print(self.myCounter)
         
+    # Decrement counter and update LCD in window   
     def subtract(self):
         self.myCounter = self.myCounter - 1
         self.lcd.display(self.myCounter)
         print(self.myCounter)
         
 class Example(QWidget):  
+    sliderW = 0
+    
     def __init__(self):
         super().__init__()
         
@@ -62,6 +94,8 @@ class Example(QWidget):
     def initUI(self):
         # Instead of resize() and move(), setGeometry() can be used to set both
         #self.setGeometry(300, 300, 300, 220)
+                  
+        self.sliderW = SliderWindow()
                            
         # Set size of the window
         self.resize(250, 150)
@@ -78,6 +112,9 @@ class Example(QWidget):
         btn2 = QPushButton("Toggle Window", self)
         btn2.clicked.connect(self.buttonClicked)
         
+        btn3 = QPushButton("Show Slider", self)
+        btn3.clicked.connect(self.openSlider)
+        
         plus = QPushButton("+", self)
         plus.clicked.connect(self.add)
         
@@ -88,6 +125,7 @@ class Example(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(plus)
         vbox.addWidget(minus)
+        vbox.addWidget(btn3)
         vbox.addWidget(btn2)
         vbox.addWidget(btn)
         self.setLayout(vbox)
@@ -113,9 +151,13 @@ class Example(QWidget):
             self.dialog.close()
         else:
             self.dialog.show()
-            
+    
+    def openSlider(self):
+        self.sliderW.show()
+                    
     def add(self):
         self.dialog.add()
+        
     def subtract(self):
         self.dialog.subtract()
         
