0f73b332bbe0a5e62550ac4a008ad5b9fa06d768
David Mueller <dave.mueller@gmx.net>
2018-11-08 12:00:58 +0100

    Implemented rockPaperScissors strategy: switch on lose

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -14,15 +14,51 @@ Game outcome:
 
 import random
 
-# Generate random plays for opponent
-def opponentTurns(turns):
-    plays = [0]*turns
-    for x in range(0, turns):
-        # Roll a random value: 0, 1 or 2
-        plays[x] = random.randrange(0, 3)
+#------------------------------------------------------------
+# Global Variables
+#------------------------------------------------------------
+opponentsLastPlay = random.randrange(0, 3)
+playStyle = 1
+style = 1 #random.randrange(0,2)
+
+#------------------------------------------------------------
+# My Functions
+#------------------------------------------------------------
+
+# Parse plays (0, 1, 2) into strings (Rock, Paper, Scissors)
+def parseToString(plays):
+    outputString = []
+    for play in plays:
+        if( play == 0):
+            outputString.append('Rock')
+        elif (play == 1):
+            outputString.append('Paper')
+        elif (play == 2):
+            outputString.append('Scissors')
+    print(outputString)
     
-    print(plays)
-    return plays
+# Generate plays for opponent (either by random, or with a strategy)
+def opponentTurns(lost):
+    play = 0
+    # Select strategi for opponent
+    if (playStyle == 0):
+        # Roll a random value: 0, 1 or 2
+        play = random.randrange(0, 3)
+    else:
+        if (style == 1):
+            global opponentsLastPlay
+            print(opponentsLastPlay)
+            play = opponentsLastPlay
+            if (lost == 1):
+                print('Bot Lost, change symbol')
+                play = random.randrange(0, 3)
+                while (play == opponentsLastPlay):
+                    play = random.randrange(0, 3)
+                opponentsLastPlay = play
+        else:
+            playerLastPlay = 0
+            play = playerLastPlay
+    return play
 
 # Input of plays of Player 1
 def myTurn():
@@ -47,12 +83,14 @@ def calculateWinner(gameResults):
 
 # Loop for every turn of the game and calculate the outcome of the round
 def startGame(turns):
-    otherPlays = opponentTurns(turns)
-    myPlays = [0] * turns
     outcome = [0] * turns
+    myPlays = [0] * turns
+    otherPlays = [0] * turns
+    otherPlays[0] = opponentTurns(0)
+    
     for x in range (0, turns):
         myPlays[x] = myTurn()
-        
+                
         # Determine who has won this round
         if(otherPlays[x] == myPlays[x]):
             outcome[x] = 0
@@ -72,9 +110,19 @@ def startGame(turns):
                     outcome[x] = 1
                 else:
                     outcome[x] = -1
+        
+        # Calculate opponents next turn
+        if (x+1 < turns):
+            otherPlays[x+1] = opponentTurns(outcome[x])
+            
+    parseToString(myPlays)
+    parseToString(otherPlays)
+    print(outcome)
     return outcome
 
-# Program starts:
+#------------------------------------------------------------
+# Program start
+#------------------------------------------------------------
 print('Welcome to Rock-Paper-Scissors! \nHow many rounds do you like to play?')
 turnSelect = int(input('Turns: '))
 outcome = startGame(turnSelect)
